2290fd6c13742f8a6cc634bfba6b871e90e7f64d
peacepete <peacepeters2@gmail.com>
2022-03-18 13:31:40 +0100

    Initial

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,12 +2,12 @@
 /**
  * print_last_digit - prints the last digit of a number.
  * @n: digit to find the last place of.
- * Return: The last digit.
+ * Return: The last digit of number.
  */
-int print_last_digit(INT_MIN)
+int print_last_digit(int n)
 {
 	if (n < 0)
-		n = n * -1;
+		n * -1;
 	_putchar((n % 10) + '0');
 	return (n % 10);
 }
