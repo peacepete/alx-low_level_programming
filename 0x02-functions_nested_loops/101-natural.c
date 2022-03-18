6c416b336a5367636aceeebc062809b66f8f6be9
peacepete <peacepeters2@gmail.com>
2022-03-18 15:08:52 +0100

    Initial

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - lists natural numbers below 1024 that are x of 3 or 5
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i, sum = 0;
+
+	for (i = 0; i < 1024; i++)
+	{
+		if ((i % 3) == 0 || (i % 5) == 0)
+			sum += i;
+	}
+
+	printf("%d\n", sum);
+
+	return (0);
+}
